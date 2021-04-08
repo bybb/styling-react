0cb2b69e889303f537ee0963e8acdb1920d1a5f0
pkyy <ai9gle@gmail.com>
2021-04-08 23:53:57 +0900

    classnames

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import styles from './CSSModule.module.css';
+import classNames from 'classnames/bind';
+import styles from './CSSModule.module.scss';
+
+const cx = classNames.bind(styles);
+
 const CSSModule = () => {
   return (
     //<div className={`${styles.wrapper} ${styles.inverted}`}>
-    <div className={[styles.wrapper, styles.inverted].join(' ')}>  
+    <div className={cx('wrapper', 'inverted')}>  
       안녕하세요, 저는 <span className="something">CSS Module</span>
     </div>
   );
